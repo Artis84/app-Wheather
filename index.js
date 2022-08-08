4536ccd36368727cf6fcd85f15e6503374fd22ec
Artis84 <45531582+Artis84@users.noreply.github.com>
2022-08-08 14:58:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 var citySet = "Avignon";
 
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q="+ citySet +"&units=metric&appid=f124eb91dc884aba7307bdedb2e78c99", function(data){
+$.getJSON("//api.openweathermap.org/data/2.5/weather?q="+ citySet +"&units=metric&appid=f124eb91dc884aba7307bdedb2e78c99", function(data){
 	console.log(data);
 
-	var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+	var icon = "//openweathermap.org/img/w/" + data.weather[0].icon + ".png";
 	
 	var temp = Math.floor(data.main.temp)
 
